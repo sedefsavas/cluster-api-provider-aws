370f8b6babbbbc05c76e1addd37f6f9608f748a9
Sedef <ssavas@vmware.com>
2020-12-10 12:21:35 -0500

    Important fix about adding the same context in main

--- main.go
+++ main.go
@@ -242,7 +242,7 @@ func main() {
 	}
 
 	setupLog.Info("starting manager", "version", version.Get().String())
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+	if err := mgr.Start(ctx); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
