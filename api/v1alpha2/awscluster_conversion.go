ea097100a8b552c08ec25bf12ef1939430498d8f
Andrew Myhre <andrew.myhre@gmail.com>
2021-02-16 10:45:42 -0500

    Implement AWSClusterRolePrincipal and AWSClusterStaticPrincipal handling

--- api/v1alpha2/awscluster_conversion.go
+++ api/v1alpha2/awscluster_conversion.go
@@ -100,6 +100,7 @@ func (src *AWSCluster) ConvertTo(dstRaw conversion.Hub) error {
 	// Manually convert conditions
 	dst.SetConditions(restored.GetConditions())
 
+	dst.Spec.PrincipalRef = restored.Spec.PrincipalRef
 	return nil
 }
 
